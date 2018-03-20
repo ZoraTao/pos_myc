0e8c873df05fc6f8605dde8050925a9f64d25540
taotao <870370433@qq.com>
2018-03-20 17:56:54 +0800

    add  member

---
+++ src/components/MemberManage/MemberIndex/member-index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+sdfdf
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "memberIndex",
+      data () {
+        return {}
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Layout from '@/components/Layout'
 import Login from '@/components/Login'
+import MemberIndex from '@/components/MemberManage/MemberIndex/member-index'
 
 Vue.use(Router)
 
@@ -16,6 +17,17 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    //会员管理
+    {
+      path: '/member/', component: MemberIndex,
+      children: [
+        //会员首页
+        {
+          path: 'memberIndex',
+          component: MemberIndex
+        }
+      ]
     }
   ]
 })
