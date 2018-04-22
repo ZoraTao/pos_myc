b37f0c9163430215945c0cfdc54f534bf67eded2
taotao <870370433@qq.com>
2018-04-22 13:41:12 +0800

    首页--公告、促销活动

--- config/index.js
+++ config/index.js
@@ -14,7 +14,6 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    // Various Dev Server settings10.0.17.68
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
